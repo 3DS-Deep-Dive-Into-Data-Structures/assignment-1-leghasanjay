48af07fed810dadabd36e61e24e831e113fdbcdf
leghasanjay <153372065+leghasanjay@users.noreply.github.com>
2024-06-04 15:31:14 +0530

    Add files via upload

---
+++ maxsubarraysum.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n, a, b;
+    cin >> n >> a >> b;
+    multiset<int> s;
+
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        s.insert(x);
+    }
+
+    auto it = s.begin();
+    int count = 0;
+    // while(count<s.size()){
+    //     it++;
+    //     count++;
+    // }
+    // count=0;
+    auto itr = s.begin();
+    while (count < n - (b - a) - 1)
+    {
+        count++;
+        itr++;
+    }
+    int sum = 0;
+    while (itr != s.end())
+    {
+        sum += *itr;
+        itr++;
+    }
+    cout << sum << endl;
+
+    return 0;
+}
